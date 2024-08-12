2f85cc60192761cf821b89aadc68088de7c91627
moosa sahad <sahadmoosa018@gmail.com>
2024-08-12 12:36:17 +0530

    creat theem app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+
+import './App.css';
+import { Handel } from './component/function';
+import { Clashand } from './component/classcomponent';
+function App() {
+  return (
+    <div className="App">
+      <Handel/>
+      <Clashand/>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 6f06634 (Initialize project using Create React App)
+    </div>
+  );
+}
+
+export default App;
